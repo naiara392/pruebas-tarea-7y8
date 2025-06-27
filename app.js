5ce9596ae742d4fc815ab2f623c02d7d94df21c3
Naiara392 <naiarapalacio@gmail.com>
2025-06-27 09:03:20 +0200

    mejoras en la interfaz de usuario en app (v1.1.0)

--- app.js
+++ app.js
@@ -3,3 +3,5 @@ console.log("Aplicación v1.0.0 iniciada.");
 console.log("Funcionalidad base implementada.");
 
 console.log("Nueva característica: Soporte para configuración básica.");
+
+console.log("Inicio de módulo de interfaz de usuario mejorado.");
