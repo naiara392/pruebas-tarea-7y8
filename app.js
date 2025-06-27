7b37a9b322fc2c821eabf15f92031e8bc5bd7c05
Naiara392 <naiarapalacio@gmail.com>
2025-06-27 08:50:13 +0200

    mejoras en app y utils (v1.0.1)

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
 // app.js
 console.log("Aplicación v1.0.0 iniciada.");
 console.log("Funcionalidad base implementada.");
+
+console.log("Nueva característica: Soporte para configuración básica.");
--- utils.js
+++ utils.js
@@ -4,3 +4,8 @@ function add(a, b)
     return a + b;
 }
 // Función de utilidad básica
+
+function subtract(a, b) 
+{                       // Nueva función, simula una "corrección" o mejora en utilidades
+    return a - b;
+}
